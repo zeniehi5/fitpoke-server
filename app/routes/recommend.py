6392037f5c1abfa39c4451ef3fef196ad4e49052
zeniehi5 <zeniehi5@gmail.com>
2025-06-27 18:25:05 -0700

    fix: correct typo

--- app/routes/recommend.py
+++ app/routes/recommend.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.schemas import RecommendResponse, RecommendRequest
-from app.services.calculator import calculate_bmi, calculate_recommended_intake
+from app.services.calculator import calculate_bmi, calculate_recommended_intake, calculate_bmr
 from app.services.recommender import recommend_menus_by_calories
 
 router = APIRouter()
@@ -9,14 +9,14 @@ router = APIRouter()
 @router.post("/recommend", response_model=RecommendResponse)
 def recommend(data: RecommendRequest):
     bmi = calculate_bmi(data.weight, data.height)
-    bmr = calculate_bmi(data.gender, data.weight, data.height, data.age)
+    bmr = calculate_bmr(data.gender, data.weight, data.height, data.age)
     recommended_intake = calculate_recommended_intake(bmr)
 
-    recommended_menus = recommend_menus_by_calories(recommended_intake)
+    recommended_menu = recommend_menus_by_calories(recommended_intake)
 
     return RecommendResponse(
         bmi=bmi,
         bmr=bmr,
         recommended_intake=recommended_intake,
-        recommended_menu=recommended_menus
+        recommended_menu=recommended_menu
     )
